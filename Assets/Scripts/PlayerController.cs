f329c86fbd19219a7204a6601fbfae3a5799c3b8
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2023-09-07 18:24:47 -0700

    primer push

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Movimiento")]
+    public float moveSpeed;
+
+    [Header("Salto")]
+    private bool canDoubleJump;
+    public float jumpForce;
+
+
+    [Header("Componentes")]
+    public Rigidbody2D theRB;
+
+    [Header("Grounded")]
+    private bool isGrounded;
+    public Transform groundCheckpoint;
+    public LayerMask whatIsGround;
+
+    [Header("Animator")]
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckpoint.position, .2f, whatIsGround);
+
+        if(isGrounded) {
+            canDoubleJump = true;
+        }
+
+        if(Input.GetButtonDown("Jump")){
+            if(isGrounded) {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }else
+            {
+                if(canDoubleJump){
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+            
+        }
+
+        anim.SetFloat("moveSpeed", theRB.velocity.x);
+    }
+}
