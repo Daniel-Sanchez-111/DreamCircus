a190bbe8f072fb0a2ebd0290cd20f6852099fc49
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2023-10-30 00:47:23 -0700

    segundo minijuego

--- Assets/Scripts/HangmanMinigame.cs
+++ Assets/Scripts/HangmanMinigame.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class HangmanMinigame : MonoBehaviour
 {
-    string[] words = { "palabra1", "palabra2", "palabra3" };
+    string[] words = { "Circo", "Payaso", "Carpa", "Elefante", "Caballo", "Leon", "Espectaculo", "Diversion", "Funcion", "Amistad", "Domador", "Malabarista", "Magia", "Acrobata", "Aplauso", "Pista"};
 
     void Start()
     {
+        int randomWord = Random.Range(1, 4);
         Debug.Log(words[1]);
         Debug.Log(words[2]);
         Debug.Log(words[2].Length); 
